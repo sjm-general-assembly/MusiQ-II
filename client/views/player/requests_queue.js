bbbb22a34506246393dfa7a9303a4d20a5e4d7ab
stevenjmarsh <sjm.atl@gmail.com>
2014-03-23 13:10:11 -0700

    first commit, displaying some sample data

---
+++ client/views/player/requests_queue.js
@@ -0,0 +1,22 @@
+var requestsData = [
+  {
+    title: "Mo' Horizons ~ Gonna Be [Ben Human Mix]",
+    videoId: "qzeaHQbg4uc"
+  },
+  {
+    title: "Taj Mahal - Shady Grove",
+    videoId: "oShuuDsXHQE"
+  },
+  {
+    title: "Dizzy Gillespie - Manteca (Funky Lowlives Extended Mix)",
+    videoId: "IOuysxKp2Ns"
+  },
+  {
+    title: "Jeep's Blues",
+    videoId: "uUcEGOLfUTE"
+  }
+];
+
+Template.requestsQueue.helpers({
+  requests: requestsData
+});
