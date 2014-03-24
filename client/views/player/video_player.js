85037dc3d580ab1423808de58e55cd269015b922
stevenjmarsh <sjm.atl@gmail.com>
2014-03-24 16:47:59 -0700

    re-orging some yt player code, no functionality changes

--- client/views/player/video_player.js
+++ client/views/player/video_player.js
@@ -1,3 +1,29 @@
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var DEFAULT_STANDBY_VIDEO = 'Q98_0Af8tG8'; 
+var YT_Player;
+
+// The API will call this function when the video player is ready.
+var onPlayerReady = function(event) {
+  // if default / please stand by video, don't start it, let it stay paused (so it's quiet)
+  event.target.playVideo();
+};
+
+// The API calls this function when the player's state changes.
+// The function indicates that when playing a video (state=1),
+// or the player ended (state=0).
+var onPlayerStateChange = function(event) {
+  if (event.data == YT.PlayerState.ENDED) {
+    var nextVideo = Requests.findOne({status: 'next'}).videoId;
+    YT_player.loadVideoById(nextVideo);
+    //window.location.replace("/play_next");  // plays next track in queue
+  }
+};
+
 Template.videoPlayer.rendered = function() {
 
   var currentVideo = Requests.findOne({status: 'waiting'}).videoId;
@@ -15,30 +41,3 @@ Template.videoPlayer.rendered = function() {
     }
   });
 };
-
-// This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var DEFAULT_STANDBY_VIDEO = 'Q98_0Af8tG8';
-var YT_Player;
-
-// The API will call this function when the video player is ready.
-onPlayerReady = function(event) {
-  // if default / please stand by video, don't start it, let it stay paused (so it's quiet)
-  event.target.playVideo();
-};
-
-// The API calls this function when the player's state changes.
-// The function indicates that when playing a video (state=1),
-// or the player ended (state=0).
-onPlayerStateChange = function(event) {
-  if (event.data == YT.PlayerState.ENDED) {
-    var nextVideo = Requests.findOne({status: 'next'}).videoId;
-    YT_player.loadVideoById(nextVideo);
-    //window.location.replace("/play_next");  // plays next track in queue
-  }
-};
-
