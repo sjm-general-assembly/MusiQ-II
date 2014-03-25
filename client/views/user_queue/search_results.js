d66d393c9fb5d0904e19cc3e6d409b77f69a7880
stevenjmarsh <sjm.atl@gmail.com>
2014-03-25 16:50:00 -0700

    saving selected song to music q

--- client/views/user_queue/search_results.js
+++ client/views/user_queue/search_results.js
@@ -6,3 +6,18 @@ Template.searchResults.results = function() {
   musiqApp_searchResults_dep.depend();
   return musiqApp_searchResults;
 };
+
+Template.searchResults.events({
+  'click .addSong': function(e) {
+    e.preventDefault();
+    // save selected song
+    Requests.insert({
+      title: this.title,
+      videoId: this.videoId,
+      status: "waiting"
+    });
+    // clear current search results list, and signal data change
+    musiqApp_searchResults.length = 0;
+    musiqApp_searchResults_dep.changed();
+  }
+});
