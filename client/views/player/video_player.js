7afccde3a299c3e4d374a81c456d14e4d5c0e940
stevenjmarsh <sjm.atl@gmail.com>
2014-03-25 18:45:36 -0700

    adding support for no queued songs, play stand by video

--- client/views/player/video_player.js
+++ client/views/player/video_player.js
@@ -8,12 +8,12 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var YT_Player;
-
 // The API will call this function when the video player is ready.
 var onPlayerReady = function(event) {
   // if default / please stand by video, don't start it, let it stay paused (so it's quiet)
-  event.target.playVideo();
+  if (musiqApp_getCurrentCuedVideoId() !== musiqApp_DEFAULT_STANDBY_VIDEO) {
+    event.target.playVideo();
+  }
 };
 
 // The API calls this function when the player's state changes.
@@ -22,7 +22,7 @@ var onPlayerReady = function(event) {
 var onPlayerStateChange = function(event) {
   if (event.data == YT.PlayerState.ENDED) {
     var nextVideo = musiqApp_getNextSong();
-    YT_player.loadVideoById(nextVideo);
+    musiqApp_YT_player.loadVideoById(nextVideo);
   }
 };
 
@@ -31,7 +31,7 @@ Template.videoPlayer.rendered = function() {
 
   // This function creates an <iframe> (and YouTube player)
   // after the API code downloads.
-  YT_player = new YT.Player('ytPlayer', {
+  musiqApp_YT_player = new YT.Player('ytPlayer', {
     //height: '312',   // dimensions set in iframe, when adding the element yourself
     //width: '512',     // OR, when using response-video.css, not needed
     //videoId: 'qzeaHQbg4uc',
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,25 +1,25 @@
-if (Requests.find().count() === 0) {
-  Requests.insert({
-    title: "Mo' Horizons ~ Gonna Be [Ben Human Mix]",
-    videoId: "qzeaHQbg4uc",
-    status: "waiting"
-  });
+// if (Requests.find().count() === 0) {
+//   Requests.insert({
+//     title: "Mo' Horizons ~ Gonna Be [Ben Human Mix]",
+//     videoId: "qzeaHQbg4uc",
+//     status: "waiting"
+//   });
 
-  Requests.insert({
-    title: "Taj Mahal - Shady Grove",
-    videoId: "oShuuDsXHQE",
-    status: "waiting"
-  });
+//   Requests.insert({
+//     title: "Taj Mahal - Shady Grove",
+//     videoId: "oShuuDsXHQE",
+//     status: "waiting"
+//   });
 
-  Requests.insert({
-    title: "Dizzy Gillespie - Manteca (Funky Lowlives Extended Mix)",
-    videoId: "IOuysxKp2Ns",
-    status: "next"
-  });
+//   Requests.insert({
+//     title: "Dizzy Gillespie - Manteca (Funky Lowlives Extended Mix)",
+//     videoId: "IOuysxKp2Ns",
+//     status: "next"
+//   });
 
-  Requests.insert({
-    title: "Jeep's Blues",
-    videoId: "uUcEGOLfUTE",
-    status: "waiting"
-  });
-}
+//   Requests.insert({
+//     title: "Jeep's Blues",
+//     videoId: "uUcEGOLfUTE",
+//     status: "waiting"
+//   });
+// }
