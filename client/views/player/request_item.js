1bebf6687e4024525279fdbdf0dd36808f5ca4cc
stevenjmarsh <sjm.atl@gmail.com>
2014-04-02 10:51:27 -0700

    displaying player requests using a table

--- client/views/player/request_item.js
+++
@@ -1,6 +0,0 @@
-Template.requestItem.helpers({
-  createdFormatted: function() {
-    var dt = moment(this.created);
-    return dt.format('MM-DD h:mma');
-  }
-});
--- client/views/player/request_list.js
+++ client/views/player/request_list.js
@@ -8,5 +8,9 @@ Template.requestList.helpers({
   requestsCount: function() {
     return Requests.find({player: Meteor.user().username, $or: [{status: musiqApp_STATUS_WAITING}, 
                                 {status: musiqApp_STATUS_NOW_PLAYING}]}).count();
+  },
+  timeDateFormatted: function() {
+    var dt = moment(this.created);
+    return dt.format('MM-DD h:mma');
   }
 });
